7cbda46d8a8e03b5a24ee89472531e80118e06b2
O-verlord <rishavmasih@gmail.com>
2022-02-13 18:19:08 +0530

    language file generation was setup

--- backend/generatecode.js
+++ backend/generatecode.js
@@ -1,12 +1,16 @@
 const fs = require('fs');
 const path = require('path');
-const {v4}
+const {v4: uuid} = require("uuid")
 
 const dircodes = path.join(__dirname,"codes");
 if(!fs.existsSync(dircodes)){
     fs.mkdirSync(dircodes, {recursive: true});
 }
-const generatecode =  (format, code)=>{
-
+const generatecode = async(format, content)=>{
+    const jobid = uuid();
+    const filename = `${jobid}.${format}`
+    const filepath = path.join(dircodes, filename);
+    await fs.writeFileSync(filepath,content);
+    return filepath;
 };
  module.exports.generatecode = generatecode; 
--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,24 @@
 const express = require('express');
+const {generatecode} = require("./generatecode")
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.get('/',(req, res)=>{
-    return res.json({hello:"world!"});
+
+app.post('/run', async(req, res)=>{
+    
+    const { language = "cpp", code } = req.body;
+    
+    if(code===undefined){return res.status(400).json({success:false, error:"Empty code body!"})};
+    console.log(req.body);
+    
+    const filepath =  await generatecode(language,code);
+    return res.json({ filepath });
 });
 
-app.post('/run', (req, res)=>{
-    const { language = "cpp", code } = req.body;
-    if(code===undefined){return res.status(400).json({success:false, error:"Empty code body!"})}
-    console.log(req.body);
-    return res.json({language, code});
+app.get('/',async(req, res)=>{
+    res.send("hello'");
 });
 
 app.listen(5000, ()=>{
